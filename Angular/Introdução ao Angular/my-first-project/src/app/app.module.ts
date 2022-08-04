3bf443467ed3506495eab3e9d315756033925c14
Vanderlei Queiroz <derley.queiroz@gmail.com>
2022-08-04 19:23:09 -0300

    Pasta Angular atualizada

---
+++ Angular/Introdução ao Angular/my-first-project/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
